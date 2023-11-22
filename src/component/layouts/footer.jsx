caa6afbcd63981fe548c6fc00f34d867513a5a0d
Vanvansys <irvanansya@gmail.com>
2023-11-22 15:05:23 +0700

    menambah link icon github

--- src/component/layouts/footer.jsx
+++ src/component/layouts/footer.jsx
@@ -7,9 +7,10 @@ export default function Footer() {
         <Facebook style={'rounded-xl'} />
         <TwitterX style={'rounded-xl'} />
         <Instagram style={'rounded-xl'} />
-        <GitHub 
-          style={'rounded-xl'} 
-          link={'https://github.io/Vanvansys'} />
+        <GitHub
+          style={'rounded-xl'}
+          link={'https://github.com/Vanvansys'}
+        />
         <WhatsApp style={'rounded-xl'} />
       </div>
       <div className='text-md flex justify-center py-6 font-tinos dark:text-slate-200'>
